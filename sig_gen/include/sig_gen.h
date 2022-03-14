88151ce2f5f0e3bf11bfeb59821b9a1a81482af9
Kasper Nyhus Kaae <kanyhus@gmail.com>
2022-03-14 21:23:45 +0100

    Sine Gen working

--- sig_gen/include/sig_gen.h
+++ sig_gen/include/sig_gen.h
@@ -4,26 +4,21 @@
 #include <stdint.h>
 #include <string.h>
 
-#define MONO 1
-#define STEREO 2
-
 
 typedef struct {
-    uint8_t ch_fmt;
     uint8_t bytes_per_sample;
     uint16_t sample_rate;
 
     float _amplitude;
     float _freq;
     float _time;
-    float _deltaTime;
+    double _deltaTime;
     float _phase;
-    float _double_pi;
+    double _double_pi;
 } sig_gen_t;
 
 
 typedef struct {
-    uint8_t ch_fmt;
     uint8_t bytes_per_sample;
     uint16_t sample_rate;
     float amplitude;
@@ -32,10 +27,9 @@ typedef struct {
 } sig_gen_config_t;
 
 
-
 void sig_gen_init(sig_gen_t *sg, const sig_gen_config_t *cfg);
 void sig_gen_output(const sig_gen_t *sg, int32_t *out_data, size_t samples);
-
+void sig_gen_output_combine(sig_gen_t *sg_l, sig_gen_t *sg_r, int32_t *out_data, size_t samples);
 
 int32_t _sig_gen_get_sample(sig_gen_t *sg);
 
--- sig_gen/sig_gen.c
+++ sig_gen/sig_gen.c
@@ -7,12 +7,11 @@
 
 void sig_gen_init(sig_gen_t *sg, const sig_gen_config_t *cfg)
 {
-    sg->ch_fmt = cfg->ch_fmt;
     sg->bytes_per_sample = cfg->bytes_per_sample;
     sg->sample_rate = cfg->sample_rate;
-    sg->_amplitude = 32767.0 * cfg->amplitude;
+    sg->_amplitude = (pow(2,cfg->bytes_per_sample*8)/2)-1 * cfg->amplitude;
     sg->_freq = cfg->freq;
-    sg->_deltaTime = 1.0 / sg->sample_rate;
+    sg->_deltaTime = (1.0 / cfg->sample_rate);
     sg->_phase = cfg->phase;
     sg->_time = 0.0;
     sg->_double_pi = 2.0*M_PI;
@@ -21,25 +20,37 @@ void sig_gen_init(sig_gen_t *sg, const sig_gen_config_t *cfg)
 }
 
 
+// Outputs a mono sine
 void sig_gen_output(const sig_gen_t *sg, int32_t *out_data, size_t samples)
 {
     uint16_t l_sample;
     uint16_t r_sample;
-    
+
     for(int i=0;i<samples;i++) {
-        l_sample = get_next_sample();
-        // l_sample = _sig_gen_get_sample(sg);
-        r_sample = l_sample;
+        out_data[i] = _sig_gen_get_sample(sg);
+    }
+}
+
+// Takes two signal_gen objs and produce a stereo sine
+void sig_gen_output_combine(sig_gen_t *sg_l, sig_gen_t *sg_r, int32_t *out_data, size_t samples)
+{
+    uint16_t l_sample;
+    uint16_t r_sample;
+
+    for(int i=0;i<samples;i++) {
+        l_sample = (uint16_t)_sig_gen_get_sample(sg_l);
+        r_sample = (uint16_t)_sig_gen_get_sample(sg_r);
         // Combine l & r
         out_data[i] = (r_sample<<16)|(l_sample & 0x0000ffff);
     }
 }
 
 
+// Calculate sine sample
 int32_t _sig_gen_get_sample(sig_gen_t *sg)
 {
     float angle = sg->_double_pi * sg->_freq * sg->_time + sg->_phase;
     float result = sg->_amplitude * sin(angle);
     sg->_time += sg->_deltaTime;
-    return (int16_t)result;
+    return (int32_t)result;
 }
